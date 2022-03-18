e86d8781015316be547fd068da256ccf4f187f90
ZhangChuqiAs <1290450579@qq.com>
2022-03-18 20:47:20 +0800

    restructure the file

--- minitensor/autograd/activation_fns.py
+++ minitensor/autograd/activation_fns.py
@@ -1,4 +1,12 @@
 """
-
-
+built in 2022.3.18
+=========================================================
+I will add the activation function with backward in here
+=========================================================
+The activation functions include:
+Relu, sigmoid, Tanh, Leaky Relu
 """
+from backward_fns import BackWard
+
+
+
